ae48642fb58385dda9205d357338ca2e47edb262
Fernando López Carballeda <flc.2056@zohomail.eu>
2019-06-03 13:09:19 +0200

    Primer paso para el ReciclerView.

---
+++ app/src/main/java/edu/cftic/sql_app/vista/ListActivity.java
@@ -0,0 +1,15 @@
+package edu.cftic.sql_app.vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.cftic.sql_app.R;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
